cde925cbaf5b2b9c633cf894b5f728a785f5f8db
KaoVannak <kaovannak009@gmail.com>
2025-07-07 00:48:17 +0700

    add js

---
+++ script.js
@@ -0,0 +1,40 @@
+function imagedisplay1(){
+    document.getElementById("img").setAttribute('src','img/img1.jpg');
+    document.getElementById("border1").style.border = "1px solid black";
+    document.getElementById("border2").style.border = "none";
+    document.getElementById("border3").style.border = "none";
+    document.getElementById("border4").style.border = "none";
+    document.getElementById("border5").style.border = "none";
+}
+function imagedisplay2(){
+    document.getElementById("img").setAttribute('src','img/img2.jpg');
+    document.getElementById("border2").style.border = "1px solid black";
+    document.getElementById("border1").style.border = "none";
+    document.getElementById("border3").style.border = "none";
+    document.getElementById("border4").style.border = "none";
+    document.getElementById("border5").style.border = "none";
+}
+function imagedisplay3(){
+    document.getElementById("img").setAttribute('src','img/img3.jpg');
+    document.getElementById("border3").style.border = "1px solid black";
+    document.getElementById("border1").style.border = "none";
+    document.getElementById("border2").style.border = "none";
+    document.getElementById("border4").style.border = "none";
+    document.getElementById("border5").style.border = "none";
+}
+function imagedisplay4(){
+    document.getElementById("img").setAttribute('src','img/img4.jpg');
+    document.getElementById("border4").style.border = "1px solid black";
+    document.getElementById("border1").style.border = "none";
+    document.getElementById("border2").style.border = "none";
+    document.getElementById("border3").style.border = "none";
+    document.getElementById("border5").style.border = "none";
+}
+function imagedisplay5(){
+    document.getElementById("img").setAttribute('src','img/img5.jpg');
+    document.getElementById("border5").style.border = "1px solid black";
+    document.getElementById("border1").style.border = "none";
+    document.getElementById("border2").style.border = "none";
+    document.getElementById("border3").style.border = "none";
+    document.getElementById("border4").style.border = "none";
+}
